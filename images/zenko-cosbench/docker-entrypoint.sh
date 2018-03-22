8ef2b9d6d566008ac09071683903dc29450dd26f
vrancurel <vianney.rancurel@gmail.com>
2018-03-22 23:34:04 +0000

    drivers start at 1

--- images/zenko-cosbench/docker-entrypoint.sh
+++ images/zenko-cosbench/docker-entrypoint.sh
@@ -7,21 +7,20 @@ set -e
 # DRIVERS var can accept comma separated values
 if [[ "$DRIVERS" ]]; then
     IFS="," read -ra HOST_NAMES <<< "$DRIVERS"
-    counter=0
+    counter=1
     for host in "${HOST_NAMES[@]}"; do
 	crudini --set conf/controller.conf driver${counter} name ${host}
 	crudini --set conf/controller.conf driver${counter} url http://${host}:18088/driver
 	counter=$((counter+1))
     done
     crudini --set conf/controller.conf controller drivers ${#HOST_NAMES[@]}
-    echo "Host name has been modified to ${HOST_NAMES[@]}"
-    echo "Note: In your /etc/hosts file on Linux, OS X, or Unix with root permissions, make sure to associate 127.0.0.1 with ${HOST_NAMES[@]}"
+    echo "Drivers have been updated to ${HOST_NAMES[@]}"
 fi
 
 if [[ "$LOG_LEVEL" ]]; then
     if [[ "$LOG_LEVEL" == "TRACE" || "$LOG_LEVEL" == "DEBUG" || "$LOG_LEVEL" == "INFO" || "$LOG_LEVEL" == "WARN" || "$LOG_LEVEL" == "ERROR" ]]; then
 	crudini --set conf/controller.conf controller log_level ${LOG_LEVEL}
-	crudini --set conf/driver.conf controller log_level ${LOG_LEVEL}
+	crudini --set conf/driver.conf driver log_level ${LOG_LEVEL}
         echo "Log level has been modified to $LOG_LEVEL"
     else
         echo "The log level you provided is incorrect (TRACE/DEBUG/INFO/WARN/ERROR)"
