7733ec593363f1d3c5661a0c3a19065351d551a4
vrancurel <vianney.rancurel@gmail.com>
2020-01-15 23:56:46 -0800

    feature: azure blob driver

--- images/build.sh
+++ images/build.sh
@@ -4,7 +4,7 @@
 
 BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 VCS_REF=$(git rev-parse --verify HEAD)
-VERSION=0.0.6
+VERSION=0.0.24
 
 docker build \
         -t "zenko/zenko-cosbench:${VERSION}" \
